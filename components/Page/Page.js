6649a8a996b3aa1d06bde2680c13c4d74f17a44c
Fielding Johnston <fielding@justfielding.com>
2017-12-17 12:36:08 -0600

    Remove unnecessary 'main' div wrapped around Page

--- components/Page/Page.js
+++ components/Page/Page.js
@@ -1,13 +1,14 @@
 export const Page = ({ children }) => (
-  <div className="main">
-    <div className="page">{children}</div>
-    <style jsx>{`
-      .page {
-        max-width: 500px;
-        margin-left: auto;
-        margin-right: auto;
-      }
-    `}
+  <div className="page">
+    {children}
+    <style jsx>
+      {`
+        .page {
+          max-width: 500px;
+          margin-left: auto;
+          margin-right: auto;
+        }
+      `}
     </style>
   </div>
 );
